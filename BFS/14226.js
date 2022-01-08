17c14b236f14d42f2e7546a4c8ef114e2f59b3ad
az0325 <jy960325@gmail.com>
2022-01-08 15:14:08 +0900

    bfs

---
+++ BFS/14226.js
@@ -0,0 +1,36 @@
+let input = require("fs").readFileSync("../dev/stdin", "utf8").trim().split("\n");
+// let input = require("fs").readFileSync("/dev/stdin", "utf8").trim().split("\n");
+let [num] = input.shift().split(' ').map(Number);
+
+const arr = [...new Array(1001)].map(() => new Array(1001).fill(0))
+
+function bfs() {
+    const stack = [[1, 0, 0]] // [현재위치, 클립보드, 시간]
+    arr[1][0] = 1; // (현재위치 : 1, 클립보드 : 0)
+
+    while (stack.length) {
+        const [now, clip, time] = stack.shift();
+
+        if (now === num) return time;
+        if (0 >= now || now > 1000) continue; // S의 범위는 2<=S<=1000을 가진다.
+
+        if (!arr[now][now]) { // 연산 1. 클립보드에 현재 화면의 이모티콘 수를 저장하기
+            arr[now][now] = 1;
+            stack.push([now, now, time + 1]);
+        }
+
+        if (clip && now + clip <= 1000) { // 연산 2. 클립보드에 있는 이모티콘을 화면에 붙여넣기
+            if (!arr[now + clip][clip]) {
+                arr[now + clip][clip] = 1;
+                stack.push([now + clip, clip, time + 1]);
+            }
+        }
+
+        if (!arr[now - 1][clip]) { // 연산 3. 화면의 이모티콘 중 한 개 삭제하기
+            arr[now - 1][clip] = 1;
+            stack.push([now - 1, clip, time + 1]);
+        }
+    }
+}
+
+console.log(bfs())
---
+++ BFS/2206.js
@@ -0,0 +1,47 @@
+let input = require("fs").readFileSync("../dev/stdin", "utf8").trim().split("\n");
+// let input = require("fs").readFileSync("/dev/stdin", "utf8").trim().split("\n");
+let array = input.shift().split(' ').map(Number);
+
+const N = array[0]
+const M = array[1]
+
+const arr = [...new Array(N)].map(() => input.shift().split('').map(Number))
+const visited = [...new Array(N)].map(() => [...new Array(M)].map(() => new Array(2).fill(false)))
+
+const dir = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+const stack = [[0, 0, 1, 0]]
+
+arr[0][0] = 1
+visited[0][0][0] = true
+
+function bfs() {
+    while (stack.length) {
+        const [x, y, dis, drill] = stack.shift();
+
+        if (x === N - 1 && y === M - 1) {
+            return arr[N - 1][M - 1]
+        }
+
+        for (let i = 0; i < 4; i++) {
+            const xPos = x + dir[i][0];
+            const yPos = y + dir[i][1];
+
+            if (0 <= xPos && 0 <= yPos && xPos < N && yPos < M) {
+                if (arr[xPos][yPos] === 0 && !visited[xPos][yPos][dis]) {
+                    visited[xPos][yPos][dis] = true
+                    stack.push([xPos, yPos, dis]);
+                    arr[xPos][yPos] = arr[x][y] + 1
+                } else if (arr[xPos][yPos] === 1 && dis === 0 && !visited[xPos][yPos][1]) {
+                    visited[xPos][yPos][1] = true
+                    stack.push([xPos, yPos, dis + 1, drill + 1]);
+                    arr[xPos][yPos] = arr[x][y] + 1
+                }
+            }
+        }
+    }
+
+    return -1
+}
+
+console.log(bfs());
