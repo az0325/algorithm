f9a7f41ab47563bacc86bcdf4e50a4352c52c786
변지윤 <>
2022-04-03 15:33:22 +0900

    2525.js

--- BaekJoon/BFS/16985.js
+++ BaekJoon/BFS/16985.js
@@ -7,12 +7,55 @@ const arr = Array.from({ length: 5 }, () => Array.from({ length: 5 }, () => arra
 
 // console.log(visited)
 
-const dx = [1, -1, 0, 0, 0, 0];
-const dy = [0, 0, 1, - 1, 0, 0];
-const dz = [0, 0, 0, 0, 1, - 1];
+const dirX = [1, -1, 0, 0, 0, 0];
+const dirY = [0, 0, 1, - 1, 0, 0];
+const dirZ = [0, 0, 0, 0, 1, - 1];
 
-// const stack = []
+const rotating = () => {
+    for (let i = 0; i < n; i++)
+        for (let j = 0; j < n; j++)
+            for (let k = 0; k < n; k++)
+                rotated[0][i][j][k] = input[i][j][k];
 
-// while (stack.length) {
-//     const [] = stack.shift()
-// }
+    for (let rot = 1; rot < 4; rot++) {
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                for (let k = 0; k < n; k++) {
+                    rotated[rot][i][j][k] = rotated[rot - 1][i][k][n - 1 - j];
+                }
+            }
+        }
+    }
+}
+
+const stacking = (bit) => {
+    for (let i = 0; i < n; i++) {
+        let idx = (bit & 3); bit >>= 2;
+        for (let j = 0; j < n; j++) {
+            for (let k = 0; k < n; k++) {
+                mp[i][j][k] = rotated[idx][stackingOrder[i]][j][k];
+            }
+        }
+    }
+}
+
+const bfs = () => {
+    const stack = [[0, 0, 0, 0]]
+
+    while (stack.length) {
+        const [x, y, z, count] = stack.shift()
+
+        for (let i = 0; i < 6; i++) {
+            const nxtX = x + dirX[i];
+            const nxtY = y + dirY[i];
+            const nxtZ = z + dirZ[i];
+
+            if (!bound(nxtX, nxtY, nxtZ)) continue;
+            if (dist[nxtX][nxtY][nxtZ] != -1) continue;
+            if (mp[nxtX][nxtY][nxtZ] == 0) continue;
+            if (nxtX == n - 1 && nxtY == n - 1 && nxtZ == n - 1) return dist[x][y][z] + 1;
+            dist[nxtX][nxtY][nxtZ] = dist[x][y][z] + 1;
+            q.push({ nxtX, nxtY, nxtZ });
+        }
+    }
+}
---
+++ BaekJoon/Step4/2525.js
@@ -0,0 +1,16 @@
+// const input = require("fs").readFileSync("../dev/stdin", "utf8").trim().split("\n");
+const input = require("fs").readFileSync("/dev/stdin", "utf8").trim().split("\n");
+const [hour, minute] = input.shift().split(' ').map(Number);
+const [cook] = input.shift().split(' ').map(Number);
+
+let result = { hour, minute: minute + cook }
+
+if (result.minute >= 60) {
+    result = { hour: result.hour + parseInt(result.minute / 60), minute: result.minute % 60 }
+}
+
+if (result.hour >= 24) {
+    result = { ...result, hour: result.hour - 24 }
+}
+
+console.log(result.hour + ' ' + result.minute)
