13a8a234c0394c1414936b5615d4a4820df74e5d
Byun <38072560+az0325@users.noreply.github.com>
2023-04-02 23:59:28 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.js
@@ -0,0 +1,9 @@
+const input = require("fs").readFileSync("/dev/stdin", "utf8").trim().split("\n");
+const [num] = input.shift().split(' ').map(Number);
+
+let result = 0
+for (let i = 1; i <= num; i++) {
+    result = result + i
+}
+
+console.log(result)
