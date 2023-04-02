474a55941e04b91818472dfc58dd096e3abd7d35
Byun <38072560+az0325@users.noreply.github.com>
2023-04-02 23:40:55 +0900

    Delete 프로그래머스/lv1/12944. 평균 구하기 directory

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.js
+++
@@ -1,9 +0,0 @@
-function solution(arr) {
-    var total = 0, avg = 0
-    for(var i = 0; i < arr.length; i++){
-        total += arr[i];
-        avg = total / arr.length
-    }
-    
-    return avg;
-}
