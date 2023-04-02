406f65bad9839ba2f9c6b78d2fdc067b1f6e722b
Byun <38072560+az0325@users.noreply.github.com>
2023-04-03 00:06:39 +0900

    [Silver III] Title: 2×n 타일링, Time: 196 ms, Memory: 8632 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -0,0 +1,13 @@
+let input = require("fs").readFileSync("/dev/stdin", "utf8").trim().split("\n");
+let [num] = input.shift().split(' ').map(Number);
+
+// dp정의: 2×N을 1×2, 2×1 타일로 채우는 방법의 수
+const dp = []
+dp[1] = 1
+dp[2] = 2
+
+for (let i = 3; i <= num; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+}
+
+console.log(dp[num])
