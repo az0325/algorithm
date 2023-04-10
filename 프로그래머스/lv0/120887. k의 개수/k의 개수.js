c6256d8a6d58208a5ffc05f5ec9e4b9e8047cf4f
Byun <38072560+az0325@users.noreply.github.com>
2023-04-10 15:09:43 +0900

    [level 0] Title: k의 개수, Time: 12.66 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,11 +1,10 @@
 function solution(i, j, k) {
-    let result = 0
     const list = []
     for(let x = i; x <= j; x++) {
         const number = x.toString().split('')
         list.push(...number)
     }
   
-    const total = list.map(Number).filter(item => item === k)
-    return total.length
+    const result = list.map(Number).filter(item => item === k)
+    return result.length
 }
