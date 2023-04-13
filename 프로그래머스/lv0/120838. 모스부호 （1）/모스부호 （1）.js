13ed45ec21c601144b3d68c27f2c8d86a80846fe
Byun <38072560+az0325@users.noreply.github.com>
2023-04-13 11:21:13 +0900

    [level 0] Title: 모스부호 (1), Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,17 @@
+function solution(letter) {
+    const list = letter.split(' ')
+    const morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+    
+    const result = []
+    list.forEach(item => {
+        result.push(morse[item])
+    })
+    
+    return result.join('')
+}
