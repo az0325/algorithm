94e6fecc7df3f4e86d15529b7324b6d3300ad7c4
Byun <38072560+az0325@users.noreply.github.com>
2023-09-22 10:16:33 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,5 @@
+function solution(a, b) {
+    const sum1 = Number(String(a) + String(b))
+    const sum2 = Number(String(b) + String(a))
+    return sum1 > sum2 ? sum1 : sum1 === sum2 ? sum1 : sum2
+}
